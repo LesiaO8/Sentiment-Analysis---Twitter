52ea6c941383b63b92939c1a714f9106c67a7e49
Amrit Swaroop Sinha <sinha.amrit093@gmail.com>
2019-08-26 21:27:57 +0530

    Create Sentiment_analyzer.py

---
+++ Sentiment_analyzer.py
@@ -0,0 +1,25 @@
+import tweepy
+from textblob import TextBlob
+
+consumer_key="ENTER YOUR CONSUMER KEY"
+consumer_secret="ENTER YOUR CONSUMER SECRET KEY"
+
+access_token="ENTER YOUR ACCESS TOKEN"
+access_token_secret="ENTER YOUR SECRET ACCESS TOKEN"
+
+#for authentication
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api=tweepy.API(auth)
+
+public_tweets = api.search("Weather")
+
+#output the sentiment tuple to a file and process the data per your requirements
+with open("output.txt", 'a') as the_file:
+    for tweet in public_tweets:
+        #print(tweet.text)
+        analysis = TextBlob(tweet.text)
+        #write the sentiment tuple to the output file
+        the_file.write(str(analysis.sentiment) + "\n")
